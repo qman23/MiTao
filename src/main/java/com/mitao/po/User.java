32db8cbc72d5cbacc7064ffb6118ad1f7197bcf0
McGee King <499085279@qq.com>
2017-06-13 00:59:16 +0800

    pom update

--- src/main/java/com/mitao/po/User.java
+++ src/main/java/com/mitao/po/User.java
@@ -1,5 +1,15 @@
 package com.mitao.po;
 
+import java.sql.Timestamp;
+
 public class User {
 
+	private String UserID;
+	private String Name; 
+	private String Password;
+	private String Phone;
+	private Integer Role; 
+	private Timestamp RegisterTime; 
+	private Timestamp LoginTime;
+	private String LoginIP;
 }
